b58a08b9df43b97fe2f88ee1f78f12f4dc7d011c
Finnl <Finnl@DESKTOP-7E0FGLN>
2024-09-27 14:06:55 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aml_group27.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
