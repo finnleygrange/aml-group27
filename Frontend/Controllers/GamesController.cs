46e8602193d7c596568ee49804a9fe0884146b78
Finnley <per.finnleygrange@icloud.com>
2024-11-18 15:29:59 +0000

    Seperated each media type into seperate controllers in the frontend

---
+++ Frontend/Controllers/GamesController.cs
@@ -0,0 +1,114 @@
+﻿using MediaService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+public class GamesController : Controller
+{
+    private readonly HttpClient _httpClient;
+    Uri baseAddress = new Uri("http://localhost:5150/api");
+
+    public GamesController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = baseAddress;
+    }
+
+    [HttpGet("/games")]
+    public async Task<IActionResult> GetAllGames()
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + "/media/games");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var games = JsonConvert.DeserializeObject<List<Game>>(content);
+            return View("Index", games);  
+        }
+        return View("Error");  
+    }
+
+
+    [HttpGet("/games/{id}")]
+    public async Task<IActionResult> GetGame(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/games/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var game = JsonConvert.DeserializeObject<Game>(content);
+            return View("Details", game); 
+        }
+        return View("Error");  
+    }
+
+
+    [HttpGet("/games/create")]
+    public IActionResult CreateGame()
+    {
+        return View("Create", new Game()); 
+    }
+
+    [HttpPost("/games/create")]
+    public async Task<IActionResult> CreateGame(Game game)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(game), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(_httpClient.BaseAddress + "/media/games", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllGames"); 
+        }
+        return View("Error"); 
+    }
+
+    [HttpGet("/games/edit/{id}")]
+    public async Task<IActionResult> EditGame(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/games/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var game = JsonConvert.DeserializeObject<Game>(content);
+            return View("Edit", game);  
+        }
+        return View("Error");  
+    }
+
+    [HttpPost("/games/edit/{id}")]
+    public async Task<IActionResult> EditGame(Game game)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(game), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PutAsync(_httpClient.BaseAddress + "/media/games", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllGames");  
+        }
+        return View("Error"); 
+    }
+
+    [HttpGet("/games/delete/{id}")]
+    public async Task<IActionResult> DeleteGame(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/games/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var game = JsonConvert.DeserializeObject<Game>(content);
+            return View("Delete", game);  
+        }
+        return View("Error"); 
+    }
+
+    [HttpPost("/games/delete/{id}/confirm")]
+    public async Task<IActionResult> DeleteGameConfirmed(int id)
+    {
+        var response = await _httpClient.DeleteAsync(_httpClient.BaseAddress + $"/media/games/{id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllGames");  
+        }
+        return View("Error");
+    }
+}
---
+++ Frontend/Controllers/JournalsController.cs
@@ -0,0 +1,112 @@
+﻿using MediaService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+public class JournalsController : Controller
+{
+    private readonly HttpClient _httpClient;
+    Uri baseAddress = new Uri("http://localhost:5150/api");
+
+    public JournalsController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = baseAddress;
+    }
+
+    [HttpGet("/journals")]
+    public async Task<IActionResult> GetAllJournals()
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + "/media/journals");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var journals = JsonConvert.DeserializeObject<List<Journal>>(content);
+            return View("Index", journals); 
+        }
+        return View("Error");
+    }
+
+    [HttpGet("/journals/{id}")]
+    public async Task<IActionResult> GetJournal(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/journals/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var journal = JsonConvert.DeserializeObject<Journal>(content);
+            return View("Details", journal); 
+        }
+        return View("Error");
+    }
+
+    [HttpGet("/journals/create")]
+    public IActionResult CreateJournal()
+    {
+        return View("Create", new Journal());
+    }
+
+    [HttpPost("/journals/create")]
+    public async Task<IActionResult> CreateJournal(Journal journal)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(journal), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(_httpClient.BaseAddress + "/media/journals", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllJournals");
+        }
+        return View("Error");
+    }
+
+    [HttpGet("/journals/edit/{id}")]
+    public async Task<IActionResult> EditJournal(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/journals/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var journal = JsonConvert.DeserializeObject<Journal>(content);
+            return View("Edit", journal);
+        }
+        return View("Error");
+    }
+
+    [HttpPost("/journals/edit/{id}")]
+    public async Task<IActionResult> EditJournal(Journal journal)
+    {
+        var content = new StringContent(JsonConvert.SerializeObject(journal), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PutAsync(_httpClient.BaseAddress + "/media/journals", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllJournals");
+        }
+        return View("Error");
+    }
+
+    [HttpGet("/journals/delete/{id}")]
+    public async Task<IActionResult> DeleteJournal(int id)
+    {
+        var response = await _httpClient.GetAsync(_httpClient.BaseAddress + $"/media/journals/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var journal = JsonConvert.DeserializeObject<Journal>(content);
+            return View("Delete", journal); 
+        }
+        return View("Error"); 
+    }
+
+    [HttpPost("/journals/delete/{id}/confirm")]
+    public async Task<IActionResult> DeleteJournalConfirmed(int id)
+    {
+        var response = await _httpClient.DeleteAsync(_httpClient.BaseAddress + $"/media/journals/{id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("GetAllJournals"); 
+        }
+        return View("Error");
+    }
+}
