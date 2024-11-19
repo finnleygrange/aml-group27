4e07c1be0f01b1e2f8f08fa1c283be3cbe6653ee
DaianaPopa <116299749+DaianaPopa@users.noreply.github.com>
2024-11-19 20:26:13 +0000

    add a search form

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace Frontend.Controllers
             return View();
         }
 
+        public IActionResult Search()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
