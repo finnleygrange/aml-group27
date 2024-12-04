d7574ad459120a3f00ec19ef10de54d999ea031f
Finnley Grange <per.finnleygrange@icloud.com>
2024-12-04 12:38:46 +0000

    idk

---
+++ MediaService/Tests/Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaService;
+
+[TestClass]
+public class Test
+{
+    [TestMethod]
+    public void TestThatAlwaysPasses()
+    {
+        Assert.IsTrue(true); 
+    }
+}
